555916990bae7a27032a40cc2f7d6e9d46a8ce0e
Stefan Melstad <Stefan Melstad>
2016-10-11 22:13:15 +0000

    Added test one number

--- src/test/java/is/ru/stringcalculator/Testcalculator.java
+++ src/test/java/is/ru/stringcalculator/Testcalculator.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 public class Testcalculator {
 
 	@Test
-	public void testEmptyString () {
+	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
+	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
 	}
 }
